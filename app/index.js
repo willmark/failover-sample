2d21b35ce2950e83a97db78f9b2cb27977b3fb4c
Mark Williams <mark@jomint.com>
2016-10-22 09:56:43 -0500

    testing

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.end('<h1>' + require('os').hostname + '</h1>');
+});
+
+server.on('error', function (err) {
+  console.log(err);
+});
+
+server.listen(80, function () {
+  address = server.address();
+  console.log('opened server on %j', address);
+});
+
