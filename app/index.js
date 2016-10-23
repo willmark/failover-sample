b67af7798ebc65895f9cafe0e13b9f8b398ce93c
Mark Williams <mark@jomint.com>
2016-10-23 09:31:47 -0500

    sample work

--- app/index.js
+++ app/index.js
@@ -1,15 +1,14 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.end('<h1>your server hostname here</h1>');
+  res.end('<h1>' + process.env.HOSTNAME + '</h1>');
 });
 
 server.on('error', function (err) {
   console.log(err);
 });
 
-server.listen(80, function () {
+server.listen(process.env.PORT, function () {
   address = server.address();
   console.log('opened server on %j', address);
 });
-
